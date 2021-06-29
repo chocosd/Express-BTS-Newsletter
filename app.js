8bce9b141d9067412368e9bc9098b40e3599e1ac
Steven Dix <hello@stevendix.co.uk>
2021-06-29 21:36:44 +0100

    build(project): initialize a bootstrap express project

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion: 6
+const express = require("express");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  var data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us6.api.mailchimp.com/3.0/lists/2f4bbb77f1";
+
+  const options  = {
+    method: "POST",
+    auth: "steven1:9847720cabd97b4308677b6b62c021b9-us6"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+
+
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+            res.sendFile(__dirname + "/success.html");
+        } else if (response.statusCode >= 400 && response.statusCode < 500) {
+            res.sendFile(__dirname + "/failure.html");
+        } else if (response.statusCode >= 500 && response.statusCode < 599) {
+            res.sendFile(__dirname + "/failure.html");
+        };
+
+    })
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/")
+})
+
+app.listen(port, function() {
+  console.log("Server is running on port " + port);
+});
+
+// Audience ID
+  // 2f4bbb77f1
+
+// API Key
+  // 9847720cabd97b4308677b6b62c021b9-us6
